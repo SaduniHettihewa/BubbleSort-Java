fb7af93617f0ce04513507a020435a5377352a92
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-06-10 09:53:04 +0530

    adding bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,42 @@
+
+package bubblesort;
+
+public class BubbleSort {
+
+   
+    public static void main(String[] args) {
+         int arr[] ={2,15,6,2,3,1,21,54};  
+                 
+                System.out.println("Array Before Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                System.out.println();  
+                  
+                BubbleSort(arr);
+                 
+                System.out.println("Array After Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+        
+    }
+    
+    static void BubbleSort(int[] arr) {  
+        int n = arr.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
+  
+    }  
+    
+}
